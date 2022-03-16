1b300388e938d2389c2598439244b2607663ec91
Konurbaeva <konurbaevamadina@gmail.com>
2022-03-16 23:44:59 +0100

    added countries dropdown with map, join examples

---
+++ countries_dropdown_iii/countries.js
@@ -0,0 +1,7 @@
+/**
+ * @param {string[]} countries
+ */
+ export const getDropdown = (countries) => {
+    return `<option value="">Please select</option>
+        ${countries.map(country => `<option value="${country.toLowerCase()}">${country}</option>`).join("")}`;
+}
