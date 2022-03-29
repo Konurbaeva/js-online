b3a66962fa49e81c26de94043c34eaa338e07958
Konurbaeva <konurbaevamadina@gmail.com>
2022-03-29 22:03:45 +0200

    init commit

---
+++ todo-app-project-28/todos.js
@@ -0,0 +1,17 @@
+export default class Todos {
+    // Do NOT modify the constructor
+    constructor() {
+        // we don't capture any parameters here
+        // we're defining an array of todos with two example todos
+        this.todos = [{
+            title: "Learn JavaScript",
+            category: "work"
+        }, {
+            title: "Meditate",
+            category: "personal"
+        }];
+    }
+
+    // TODO: define remaining instance methods
+
+}
